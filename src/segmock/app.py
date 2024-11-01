18d618c3dbbc12771ff4d0bc2c74dfa1dd835eab
sou-san <sou887867@gmail.com>
2024-11-01 15:42:19 +0900

    Add src/segmock/app.py

---
+++ src/segmock/app.py
@@ -0,0 +1,31 @@
+from textual.app import App
+from textual.binding import Binding
+from textual.events import Resize
+
+from segmock.blocking_screen import BlockingScreen
+from segmock.clock import ClockScreen
+from segmock.stopwatch import StopwatchScreen
+
+MIN_WIDTH = 158
+MIN_HEIGHT = 17
+
+
+class Segmock(App[None]):
+    ENABLE_COMMAND_PALETTE = False
+    MODES = {
+        "clock": ClockScreen,
+        "stopwatch": StopwatchScreen,
+    }
+    DEFAULT_MODE = "clock"
+    BINDINGS = [
+        Binding("q", "exit_app", "Exit"),
+        Binding("c", "app.switch_mode('clock')", "Clock"),
+        Binding("s", "app.switch_mode('stopwatch')", "Stopwatch"),
+    ]
+
+    async def on_resize(self, event: Resize) -> None:
+        if event.size.width < MIN_WIDTH or event.size.height < MIN_HEIGHT:
+            self.push_screen(BlockingScreen())
+
+    def action_exit_app(self) -> None:
+        self.exit()
