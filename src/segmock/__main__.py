d18f50450bc78069f5bc66caf8902f305a60d7bf
sou-san <sou887867@gmail.com>
2025-03-12 11:45:38 +0900

    Add --version and --help options

--- src/segmock/__main__.py
+++ src/segmock/__main__.py
@@ -1,6 +1,11 @@
+import click
+
 from segmock.app import Segmock
+from segmock.version import SEGMOCK_VERSION
 
 
+@click.command()
+@click.version_option(SEGMOCK_VERSION)
 def main() -> None:
     Segmock().run()
 
